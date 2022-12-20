09b11e342b67c53c7e0f73c5a11120962283f23d
adimar <adimaraisha@gmail.com>
2022-12-20 23:41:55 +0800

    testing - env

--- components/main/MainHeader.js
+++ components/main/MainHeader.js
@@ -7,7 +7,10 @@ export default function MainHeader() {
   return (
     <div className="shadow-md">
       <div className="flex items-center gap-2 max-w-xl p-2 mx-auto px-5">
-        <h1 className="text-3xl font-bold text-yellow-500">Lango</h1>
+        <h1 className="text-3xl font-bold text-yellow-500">
+          Lango
+          {"  " + process.env.NEXT_PUBLIC_PROJECT_ID}
+        </h1>
         <Img src={curLang()?.img} h="30px" w="40px" />
       </div>
     </div>
--- firebase.js
+++ firebase.js
@@ -15,16 +15,16 @@ let firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_APP_ID,
 };
 
-if (process && process.env.NODE_ENV === "production") {
-  firebaseConfig = {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDGRID,
-    appId: process.env.APP_ID,
-  };
-}
+// if (process && process.env.NODE_ENV === "production") {
+//   firebaseConfig = {
+//     apiKey: process.env.API_KEY,
+//     authDomain: process.env.AUTH_DOMAIN,
+//     projectId: process.env.PROJECT_ID,
+//     storageBucket: process.env.STORAGE_BUCKET,
+//     messagingSenderId: process.env.MESSAGING_SENDGRID,
+//     appId: process.env.APP_ID,
+//   };
+// }
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
