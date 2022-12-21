ea105b8f3d5449c1874471c5abfca3165b55bf6f
adimar <adimaraisha@gmail.com>
2022-12-21 13:03:40 +0800

    test production changes - 1st test

--- components/main/MainHeader.js
+++ components/main/MainHeader.js
@@ -9,7 +9,7 @@ export default function MainHeader() {
       <div className="flex items-center gap-2 max-w-xl p-2 mx-auto px-5">
         <h1 className="text-3xl font-bold text-yellow-500">
           Lango
-          {"  " + process.env.NEXT_PUBLIC_PROJECT_ID}
+          {/* {"  " + process.env.NEXT_PUBLIC_PROJECT_ID} */}
         </h1>
         <Img src={curLang()?.img} h="30px" w="40px" />
       </div>
